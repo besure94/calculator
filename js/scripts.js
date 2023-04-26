b9b198580cfa1c8e2882c1aac2c94e99f5807265
Brian Scherner <brianscherner@gmail.com>
2023-04-26 15:55:14 -0700

    Add functions for subtraction, multiplication, and division, with an alert message for each function.

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,36 @@ function add(number1, number2) {
 // user interface logic
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
+const sum = add(number1, number2);
 
+window.alert("The addition of your numbers equals: " + sum);
 
-window.alert(add(number1,number2));
+function subtract(number1, number2) {
+	return number1 - number2;
+}
 
-// const result = add(3, 5);
-// const outputText = "The sum is " + result + ".";
-// saySomething(outputText);
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+const sum = subtract(number1, number2);
+
+window.alert("The subtraction of your numbers equals: " + sum);
+
+function multiply(number1, number2) {
+	return number1 * number2;
+}
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+const sum = multiply(number1, number2);
+
+window.alert("The multiplication of your numbers equals: " + sum);
+
+function divide(number1, number2) {
+	return number1 / number2;
+}
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+const sum = divide(number1, number2);
+
+window.alert("The division of your numbers equals: " + sum);
