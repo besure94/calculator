314f74db840aba9afa5606adda4dc7f2abf3d022
Brian Scherner <brianscherner@gmail.com>
2023-04-26 16:00:42 -0700

    Change placement of subtraction function and alert.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,10 @@ function add(number1, number2) {
 	return number1 + number2;
 }
 
+function subtract(number1, number2) {
+	return number1 - number2;
+}
+
 // user interface logic
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
@@ -10,9 +14,9 @@ const sum = add(number1, number2);
 
 window.alert("The addition of your numbers equals: " + sum);
 
-function subtract(number1, number2) {
-	return number1 - number2;
-}
+// function subtract(number1, number2) {
+// 	return number1 - number2;
+// }
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
@@ -20,22 +24,22 @@ const sum = subtract(number1, number2);
 
 window.alert("The subtraction of your numbers equals: " + sum);
 
-function multiply(number1, number2) {
-	return number1 * number2;
-}
+// function multiply(number1, number2) {
+// 	return number1 * number2;
+// }
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const sum = multiply(number1, number2);
+// const number1 = parseInt(prompt("Enter a number:"));
+// const number2 = parseInt(prompt("Enter another number:"));
+// const sum = multiply(number1, number2);
 
-window.alert("The multiplication of your numbers equals: " + sum);
+// window.alert("The multiplication of your numbers equals: " + sum);
 
-function divide(number1, number2) {
-	return number1 / number2;
-}
+// function divide(number1, number2) {
+// 	return number1 / number2;
+// }
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const sum = divide(number1, number2);
+// const number1 = parseInt(prompt("Enter a number:"));
+// const number2 = parseInt(prompt("Enter another number:"));
+// const sum = divide(number1, number2);
 
-window.alert("The division of your numbers equals: " + sum);
+// window.alert("The division of your numbers equals: " + sum);
